a57814bc4d022e55c5b7f00854002c1a7b3d2e59
Radhitya27 <18224126@std.stei.itb.ac.id>
2025-11-07 00:07:33 -0800

    revisi

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('SEMANGAT YA MUTIA UAS-NYA').split('')
+    const titles = ('ini buat kamu').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
